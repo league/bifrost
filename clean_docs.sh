8311e4ad8e0664b8cf230a0a14331b22d5cbb0bb
MilesCranmer <miles.cranmer@gmail.com>
2017-05-29 10:45:11 +1000

    Get clean_docs to not search .git dir

--- clean_docs.sh
+++ clean_docs.sh
@@ -5,7 +5,7 @@ cp -rf _modules/* modules/
 mkdir -p sources
 cp -rf _sources/* sources/
 
-find ./ -type f -exec sed -i -e '/\(href\|src\)=\"\(\.\.\/\)\?_static/ s/_static/static/' {} \;
-find ./ -type f -exec sed -i -e '/\(href\|src\)=\"\(\.\.\/\)\?_modules/ s/_modules/modules/' {} \;
-find ./ -type f -exec sed -i -e '/\(href\|src\)=\"\(\.\.\/\)\?_sources/ s/_sources/sources/' {} \;
-find ./ -type f -exec sed -i -e '/\$\.ajax\(.*\)_sources/ s/_sources/sources/' {} \;
+find . \( -name .git \) -prune -o -type f -exec sed -i -e '/\(href\|src\)=\"\(\.\.\/\)\?_static/ s/_static/static/' {} \;
+find . \( -name .git \) -prune -o -type f -exec sed -i -e '/\(href\|src\)=\"\(\.\.\/\)\?_modules/ s/_modules/modules/' {} \;
+find . \( -name .git \) -prune -o -type f -exec sed -i -e '/\(href\|src\)=\"\(\.\.\/\)\?_sources/ s/_sources/sources/' {} \;
+find . \( -name .git \) -prune -o -type f -exec sed -i -e '/\$\.ajax\(.*\)_sources/ s/_sources/sources/' {} \;
